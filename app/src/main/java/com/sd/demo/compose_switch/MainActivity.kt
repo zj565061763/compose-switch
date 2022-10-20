96e2788c763d0b3149521f20b5028fbb980e1e09
565061763 <565061763@qq.com>
2022-10-21 01:32:10 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_switch/MainActivity.kt
+++ app/src/main/java/com/sd/demo/compose_switch/MainActivity.kt
@@ -7,17 +7,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_switch.ui.theme.AppTheme
 import com.sd.lib.compose.swch.FSwitch
+import kotlinx.coroutines.delay
 
 private const val TAG = "MainActivity"
 
@@ -39,6 +41,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Content() {
+    var isChecked by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        isChecked = false
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -47,11 +56,11 @@ fun Content() {
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         FSwitch(
-            checked = true,
-            onCheckedChange = {
-                Log.i(TAG, "onCheckedChange: $it")
-            },
-        )
+            modifier = Modifier.width(100.dp),
+            checked = isChecked,
+        ) {
+            Log.i(TAG, "onCheckedChange: $it")
+        }
     }
 }
 
