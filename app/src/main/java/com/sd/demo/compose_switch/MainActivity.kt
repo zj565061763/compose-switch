cc98f3cf5c75338c79d9f93a71ecf8da0e234ee8
zj565061763 <565061763@qq.com>
2022-12-14 00:43:34 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_switch/MainActivity.kt
+++ app/src/main/java/com/sd/demo/compose_switch/MainActivity.kt
@@ -13,11 +13,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_switch.ui.theme.AppTheme
@@ -59,60 +58,70 @@ fun Content() {
         verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
 
-
-        FSwitch {
-            logMsg { "onCheckedChange: $it" }
-        }
-
-
-        FSwitch(
-            background = {
-                FSwitchBackground(
-                    progress = it,
-                    colorChecked = Color.Red,
-                    shape = RoundedCornerShape(5.dp),
-                )
-            },
-            thumb = {
-                FSwitchThumb(shape = RoundedCornerShape(5.dp))
-            },
-        ) {
-            logMsg { "onCheckedChange: $it" }
-        }
-
-
-        FSwitch(
-            background = {
-                FSwitchBackground(
-                    modifier = Modifier.fillMaxHeight(it.coerceAtLeast(0.2f)),
-                    progress = it,
-                    shape = RoundedCornerShape(5.dp)
-                )
-            },
-            thumb = {
-                Card(
-                    modifier = Modifier
-                        .aspectRatio(1f, true)
-                        .padding(start = 0.dp, end = 2.dp, top = 2.dp, bottom = 2.dp),
-                    shape = RoundedCornerShape(5.dp),
-                    colors = CardDefaults.cardColors(containerColor = Color.White),
-                    border = BorderStroke(Dp.Hairline, Color(0xFFE3E3E3))
-                ) {}
-            },
-        ) {
-            logMsg { "onCheckedChange: $it" }
-        }
+        SampleDefault()
+        SampleCustom1()
+        SampleCustom2()
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AppTheme {
-        Content()
+private fun SampleDefault() {
+    var checked by remember { mutableStateOf(false) }
+    FSwitch(checked) {
+        logMsg { "onCheckedChange: $it" }
+        checked = it
     }
 }
 
-fun logMsg(block: () -> String) {
+@Composable
+private fun SampleCustom1() {
+    var checked by remember { mutableStateOf(false) }
+    FSwitch(
+        checked = checked,
+        background = {
+            FSwitchBackground(
+                progress = it,
+                colorChecked = Color.Red,
+                shape = RoundedCornerShape(5.dp),
+            )
+        },
+        thumb = {
+            FSwitchThumb(shape = RoundedCornerShape(5.dp))
+        },
+    ) {
+        logMsg { "onCheckedChange: $it" }
+        checked = it
+    }
+}
+
+@Composable
+private fun SampleCustom2() {
+    var checked by remember { mutableStateOf(false) }
+    FSwitch(
+        checked = checked,
+        background = {
+            FSwitchBackground(
+                modifier = Modifier.fillMaxHeight(it.coerceAtLeast(0.2f)),
+                progress = it,
+                shape = RoundedCornerShape(5.dp)
+            )
+        },
+        thumb = {
+            Card(
+                modifier = Modifier
+                    .aspectRatio(1f, true)
+                    .padding(start = 0.dp, end = 2.dp, top = 2.dp, bottom = 2.dp),
+                shape = RoundedCornerShape(5.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                border = BorderStroke(Dp.Hairline, Color(0xFFE3E3E3))
+            ) {}
+        },
+    ) {
+        logMsg { "onCheckedChange: $it" }
+        checked = it
+    }
+}
+
+inline fun logMsg(block: () -> String) {
     Log.i("FSwitch-demo", block())
 }
