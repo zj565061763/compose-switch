5be8ecf54a567a15dd833806c3e5f233e726db68
zj565061763 <565061763@qq.com>
2023-02-27 16:40:19 +0800

    kts

---
+++ lib/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+}
+
+val libGroupId = "com.sd.lib.android"
+val libArtifactId = "compose-switch"
+val libVersionName = "1.0.0-beta03"
+
+android {
+    namespace = "com.sd.lib.compose.swich"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        kotlinOptions.freeCompilerArgs += "-module-name=$libGroupId.$libArtifactId"
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.sd.composeGesture)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = libGroupId
+                artifactId = libArtifactId
+                version = libVersionName
+            }
+        }
+    }
+}
