37226b4398a87b759edbc41c4e62da9528312a21
zj565061763 <565061763@qq.com>
2023-05-06 16:26:16 +0800

    1.0.0-beta07

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-switch"
-val libVersionName = "1.0.0-beta06"
+val libVersionName = "1.0.0-beta07"
 
 android {
     namespace = "com.sd.lib.compose.swich"
