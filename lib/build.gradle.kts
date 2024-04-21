9277b364b0b1d9a7dbd642ad3c89fd2121364019
zjun <565061763@qq.com>
2024-04-21 22:42:57 +0800

    1.2.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-switch"
-val libVersion = "1.2.1"
+val libVersion = "1.2.2"
 
 android {
     namespace = "com.sd.lib.compose.swich"
